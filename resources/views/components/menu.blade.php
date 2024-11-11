1ae1c8f3b811fe7c1a33f4f49369abafc1cafb09
hommp <141468509+hommp@users.noreply.github.com>
2024-11-11 19:19:42 +0700

    sevens

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -4,45 +4,40 @@
     nav {
         font-family: 'Poppins', sans-serif;
     }
+
+    .navbar-brand img {
+        width: 50px;
+        height: auto;
+    }
 </style>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow">
-    <div class="container fw-bold">
-        <a class="navbar-brand" href="/">
-            <img src="https://cdn.freelogovectors.net/wp-content/uploads/2023/09/nike-nocta-logo-freelogovectors.net_.png"
-                alt="Brand Logo" width="50">Yezus
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        @if (!Auth::check())
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav me-auto fw-bold">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('profile') }}">Profil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('about') }}">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('contact') }}">Contact</a>
-                </li>
-
-
-            </ul>
-        </div>
-
-        <div class="d-flex">
-            <a href="{{ route('signup') }}" class="btn btn-dark me-2 fw-bold">Signup</a>
-            <a href="{{ route('signin') }}" class="btn btn-dark fw-bold">Signin</a>
-
-            @else
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top shadow">
+        <div class="container fw-bold">
+            <a class="navbar-brand" href="/" aria-label="Homepage">
+                <img src="https://cdn.freelogovectors.net/wp-content/uploads/2023/09/nike-nocta-logo-freelogovectors.net_.png"
+                    alt="Brand Logo">Yezus
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav me-auto fw-bold">
+                    @if (!Auth::check())
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('profile') }}">Profil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('about') }}">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+                    </li>
+                    @else
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="{{ route('users') }}">Data Users</a>
                     </li>
@@ -61,12 +56,17 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('posts') }}">Posts</a>
                     </li>
-
+                    @endif
                 </ul>
+                <div class="d-flex">
+                    @if (!Auth::check())
+                    <a href="{{ route('signup') }}" class="btn btn-dark me-2 fw-bold">Signup</a>
+                    <a href="{{ route('signin') }}" class="btn btn-dark fw-bold">Signin</a>
+                    @else
+                    <a href="{{ route('logout') }}" class="btn btn-dark fw-bold">Logout</a>
+                    @endif
+                </div>
             </div>
-            <a href="{{ route('logout') }}" class="btn btn-dark fw-bold">Logout</a>
-            @endif
         </div>
-    </div>
-    </div>
-</nav>
+    </nav>
+</header>
